f7c5bc99b519d3aaa21e279e24b849799d5c47ab
Jason Li <jsn.lee1030@gmail.com>
2017-06-22 14:30:44 -0400

    Add files via upload

--- lyw/demo/src/main/java/com/example/demo/DemoApplication.java
+++ lyw/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.config.JwtFilter;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Bean
+	public FilterRegistrationBean jwtFilter(){
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/rest/*");
+		return registrationBean;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
